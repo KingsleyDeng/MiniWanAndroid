460373d4a8005021d92248104f8bdc64d727febe
KingsleyDeng <460556933@qq.com>
2020-03-25 14:52:05 +0800

    详情页面展示

--- pages/web/index.js
+++ pages/web/index.js
@@ -5,14 +5,18 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    pageurl:""
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    let url = decodeURIComponent(options.url)
+    console.log(url)
+    this.setData({
+      pageurl: url
+    })
   },
 
   /**
